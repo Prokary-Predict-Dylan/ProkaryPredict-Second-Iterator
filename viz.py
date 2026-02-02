bc1333edbb3e71e1817d87aaa61b340415575425
Prokary-Predict-Dylan <prokarypredict@gmail.com>
2026-02-02 16:23:35 -0700

    Refactor barcode_view to blocks_to_figure

--- viz.py
+++ viz.py
@@ -1,36 +1,36 @@
 # viz.py
 import plotly.graph_objects as go
 
-def barcode_view(blocks):
+def blocks_to_figure(blocks):
     fig = go.Figure()
-    for b in blocks:
-        fig.add_trace(go.Scatter(
-            x=[b["start"], b["end"]],
-            y=[0, 0],
-            mode="lines",
-            line=dict(color=b["active_color"], width=8),
-            hoverinfo="text",
-            text=f"{b['label']}<br>{b['class']}<br>{b['function']}",
-            showlegend=False
-        ))
-    fig.update_layout(height=180, yaxis_visible=False)
-    return fig
 
-def stacked_view(blocks):
-    fig = go.Figure()
     for b in blocks:
-        fig.add_bar(
-            x=[b["length"]],
-            y=[1],
-            base=[b["start"]],
-            orientation="h",
-            marker=dict(color=b["active_color"]),
-            hoverinfo="text",
-            text=b["label"],
-            showlegend=False
+        fig.add_trace(
+            go.Scatter(
+                x=[b["start"], b["end"]],
+                y=[0, 0],
+                mode="lines",
+                line=dict(
+                    color=b["active_color"],
+                    width=10
+                ),
+                hoverinfo="text",
+                text=(
+                    f"<b>{b['label']}</b><br>"
+                    f"Structural: {b['class']}<br>"
+                    f"Function: {b['function']}<br>"
+                    f"Length: {b['length']} bp"
+                ),
+                showlegend=False
+            )
         )
-    fig.update_layout(barmode="stack", height=350, yaxis_visible=False)
-    return fig
 
-def blocks_to_figure(blocks, mode="barcode"):
-    return barcode_view(blocks) if mode == "barcode" else stacked_view(blocks)
+    fig.update_layout(
+        height=200,
+        margin=dict(l=20, r=20, t=20, b=20),
+        xaxis=dict(title="Genome position"),
+        yaxis=dict(visible=False),
+        plot_bgcolor="white"
+    )
+
+    return fig
