9679f37a03e54db78252f1553ecf00c289d98dd7
Prokary-Predict-Dylan <prokarypredict@gmail.com>
2026-02-16 15:12:47 -0700

    Refactor blocks_to_figure to include genome length

--- viz.py
+++ viz.py
@@ -1,36 +1,28 @@
-# viz.py
 import plotly.graph_objects as go
 
 def blocks_to_figure(blocks):
+    genome_length = max(b["end"] for b in blocks)
     fig = go.Figure()
 
     for b in blocks:
-        fig.add_trace(
-            go.Scatter(
-                x=[b["start"], b["end"]],
-                y=[0, 0],
-                mode="lines",
-                line=dict(
-                    color=b["active_color"],
-                    width=10
-                ),
-                hoverinfo="text",
-                text=(
-                    f"<b>{b['label']}</b><br>"
-                    f"Structural: {b['class']}<br>"
-                    f"Function: {b['function']}<br>"
-                    f"Length: {b['length']} bp"
-                ),
-                showlegend=False
-            )
-        )
+        fig.add_trace(go.Bar(
+            x=[b["length"]],
+            y=[1],
+            base=[b["start"]],
+            orientation="h",
+            marker=dict(color=b["active_color"]),
+            hoverinfo="text",
+            text=f"{b['label']}<br>{b['class']}<br>{b['function']}",
+            showlegend=False
+        ))
 
     fig.update_layout(
-        height=200,
-        margin=dict(l=20, r=20, t=20, b=20),
-        xaxis=dict(title="Genome position"),
-        yaxis=dict(visible=False),
-        plot_bgcolor="white"
+        barmode="stack",
+        height=350,
+        xaxis_title="Coordinate",
+        yaxis_visible=False
     )
+
+    fig.update_xaxes(range=[0, genome_length * 1.02])
 
     return fig
